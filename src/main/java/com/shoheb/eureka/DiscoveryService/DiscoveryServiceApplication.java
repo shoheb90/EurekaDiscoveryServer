10f152d8ee484b051a8d1395da9441fb9f0c4dc1
shbtechno <shbtechno@gmail.com>
2020-07-04 15:11:42 +0100

    Enabled Eureka Server configuration on port 8010

--- src/main/java/com/shoheb/eureka/DiscoveryService/DiscoveryServiceApplication.java
+++ src/main/java/com/shoheb/eureka/DiscoveryService/DiscoveryServiceApplication.java
@@ -2,8 +2,13 @@ package com.shoheb.eureka.DiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/*
 
+	@EnableEurakaServer : Used to mark this application as eureka server
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
