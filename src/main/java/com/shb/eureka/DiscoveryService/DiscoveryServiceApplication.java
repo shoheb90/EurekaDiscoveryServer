ccb2d3bb522bd25f6addef3fc5e045409111682b
shbtechno <shbtechno@gmail.com>
2020-07-04 15:51:58 +0100

    Enabled Eureka Server configuration on port 8010

--- src/main/java/com/shb/eureka/DiscoveryService/DiscoveryServiceApplication.java
+++ src/main/java/com/shb/eureka/DiscoveryService/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.shoheb.eureka.DiscoveryService;
+package com.shb.eureka.DiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/shb/eureka/DiscoveryService/DiscoveryServiceApplicationTests.java
+++ src/test/java/com/shb/eureka/DiscoveryService/DiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shoheb.eureka.DiscoveryService;
+package com.shb.eureka.DiscoveryService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
